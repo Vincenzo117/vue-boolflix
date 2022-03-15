4152759f68d3a00090b6b81b7a594528e05046bc
Vincenzo117 <94491363+Vincenzo117@users.noreply.github.com>
2022-03-15 16:09:35 +0100

    header component

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,14 @@
 <template>
   <header>
-      header
+      <div class="container">
+          <figure class="header__logo">
+              <img src="" alt="Boolflix">
+          </figure>
+          <nav class="header__nav">
+              <input type="text" class="header__searchbar" placeholder="Search a film or a series">
+              <btn class="header__search-btn">Search</btn>
+          </nav>
+      </div>
   </header>
 </template>
 
@@ -10,6 +18,39 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/scss/variables.scss";
+header {
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 20px;
 
+        .header__logo {
+            color: $accent-color;
+            font-weight: 600;
+            font-size: 30px;
+        }
+
+        .header__nav {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+
+            .header__searchbar {
+                border: 1px solid $secondary_color;
+                background-color: black;
+                color: $secondary_color;
+                padding: 10px 20px;
+            }
+
+            .header__search-btn {
+                padding: 10px 15px;
+                background-color: $accent_color;
+                cursor: pointer;
+            }
+        }
+    }
+}
 </style>
